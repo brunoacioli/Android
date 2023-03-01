7b4cd2289cb24e6ecfe3c4ef494d09e14063a549
Bruno Acioli <baal@ua.pt>
2023-03-01 01:30:25 +0000

    Dialer app

---
+++ Dialer/app/src/main/java/com/example/dialer/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.dialer
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var b1: Button
+    lateinit var b2: Button
+    lateinit var b3: Button
+    lateinit var b4: Button
+    lateinit var b5: Button
+    lateinit var b6: Button
+    lateinit var b7: Button
+    lateinit var b8: Button
+    lateinit var b9: Button
+    lateinit var b0: Button
+    lateinit var bstar: Button
+    lateinit var bhash: Button
+    lateinit var bcall: Button
+
+    lateinit var back: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        b1 = findViewById(R.id.b1)
+        b2 = findViewById(R.id.b2)
+        b3 = findViewById(R.id.b3)
+        b4 = findViewById(R.id.b4)
+        b5 = findViewById(R.id.b5)
+        b6 = findViewById(R.id.b6)
+        b7 = findViewById(R.id.b7)
+        b8 = findViewById(R.id.b8)
+        b9 = findViewById(R.id.b9)
+        b0 = findViewById(R.id.b0)
+        bstar = findViewById(R.id.bstar)
+        bhash = findViewById(R.id.bhash)
+        bcall = findViewById(R.id.bcall)
+        back = findViewById(R.id.ibtn)
+
+        Dexter.withContext(this).withPermission(Manifest.permission.CALL_PHONE).withListener(object: PermissionListener() {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?,
+                p1: PermissionToken?
+            ) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
